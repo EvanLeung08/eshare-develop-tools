ef7647390551c66b74b08434e54fea36257a3b18
Evan43789596 <10856214@163.com>
2019-08-25 18:11:45 +0800

    feat(#All): support x.509 key

--- security-tools/src/main/java/com/eshare/security/utils/KeyConvertUtil.java
+++ security-tools/src/main/java/com/eshare/security/utils/KeyConvertUtil.java
@@ -1,7 +1,14 @@
 package com.eshare.security.utils;
 
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
 import org.bouncycastle.openpgp.PGPException;
 import org.bouncycastle.openpgp.PGPPrivateKey;
 import org.bouncycastle.openpgp.PGPPublicKey;
@@ -34,6 +41,25 @@ public class KeyConvertUtil {
   public static PrivateKey convertPGPPrivateKey2PrivateKey(PGPPrivateKey pgpPrivateKey)
       throws PGPException {
     return new JcaPGPKeyConverter().getPrivateKey(pgpPrivateKey);
+  }
+
+  /**
+   * Get keypair from keystore file
+   * @param keyStore keystore file object
+   * @param alias key alias used to find te corresponding key
+   * @param password key password for the key
+   * @return the keypair
+   * @throws UnrecoverableKeyException
+   * @throws NoSuchAlgorithmException
+   * @throws KeyStoreException
+   */
+  public static KeyPair getKeyPair(final KeyStore keyStore, final String alias,
+      final String password)
+      throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException {
+    final Key key = keyStore.getKey(alias, password.toCharArray());
+    final Certificate cert = keyStore.getCertificate(alias);
+    final PublicKey publicKey = cert.getPublicKey();
+    return new KeyPair(publicKey, (PrivateKey) key);
   }
 
 }
--- security-tools/src/test/java/com/eshare/security/utils/KeyConvertUtilTest.java
+++ security-tools/src/test/java/com/eshare/security/utils/KeyConvertUtilTest.java
@@ -6,11 +6,20 @@ import io.jsonwebtoken.lang.Assert;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.KeyStoreSpi;
+import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Security;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.interfaces.RSAKey;
 import java.util.UUID;
+import org.bouncycastle.jcajce.provider.keystore.pkcs12.PKCS12KeyStoreSpi;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.PGPException;
 import org.bouncycastle.openpgp.PGPPrivateKey;
@@ -20,6 +29,7 @@ import org.bouncycastle.openpgp.PGPSecretKeyRingCollection;
 import org.bouncycastle.openpgp.PGPUtil;
 import org.bouncycastle.openpgp.operator.jcajce.JcaKeyFingerprintCalculator;
 import org.junit.Test;
+import sun.security.tools.KeyStoreUtil;
 
 /**
  * Created by liangyh on 2019/8/25. Email:10856214@163.com
@@ -31,6 +41,26 @@ public class KeyConvertUtilTest {
 
   public static final String PUBLIC_KEY_FILE = "pub.asc";
   public static final String PRIVATE_KEY_FILE = "secret.asc";
+
+  /**
+   * Key store file name
+   * <p>
+   * keytool -genkey -alias mykey20201231 -keyalg RSA -validity 145 -keystore mycompany.keystore
+   * -storetype JKS -storepass 123456
+   *
+   * keytool -importkeystore -srckeystore mycompany.keystore -srcstorepass 123456 -srckeypass 123456
+   * -srcalias mykey20201231 -destalias mykey20201231 -destkeystore mycompany.p12 -deststoretype
+   * PKCS12 -deststorepass 1234546 -destkeypass 123456
+   *
+   *
+   * keytool -export -alias mykey20201231 -keystore mycompany.keystore -file myKeyAliasInBank.cer -storepass 123456
+   * //import public key
+   * keytool -import -alias test -file myKeyAliasInBank.cer -keystore mycompany.keystore -storepass 123456
+   * </p>
+   */
+  public static final String KEYSTORE_FILE = "mycompany.keystore";
+
+  public static final String PRIVATE_KEY_ALIAS = "mykey20201231";
 
 
   /**
@@ -45,16 +75,16 @@ public class KeyConvertUtilTest {
   }
 
   @Test
-  public void convertPGPPublicKey2PublicKey() throws IOException, PGPException {
+  public void testConvertPGPPublicKey2PublicKey() throws IOException, PGPException {
     PGPPublicKey pgpPublicKey = PGPKeyUtil.readPublicKey(findFile(PUBLIC_KEY_FILE));
     PublicKey publicKey = KeyConvertUtil.convertPGPPublicKey2PublicKey(pgpPublicKey);
-    //Check if the public key is an instance of Key
-    Assert.isInstanceOf(Key.class, publicKey);
+    //Check if the public key is an instance of RSAKey
+    Assert.isInstanceOf(RSAKey.class, publicKey);
 
   }
 
   @Test
-  public void convertPGPPrivateKey2PrivateKey()
+  public void tsetConvertPGPPrivateKey2PrivateKey()
       throws IOException, PGPException, NoSuchProviderException {
     PGPSecretKey pgpSecretKey = PGPKeyUtil.readSecretKey(findFile(PRIVATE_KEY_FILE));
     //Prepare key ring collection
@@ -63,9 +93,21 @@ public class KeyConvertUtilTest {
     final PGPPrivateKey pgpPrivateKey = PGPKeyUtil
         .findSecretKey(pgpSec, pgpSecretKey.getKeyID(), PASSWORD.toCharArray());
     PrivateKey privateKey = KeyConvertUtil.convertPGPPrivateKey2PrivateKey(pgpPrivateKey);
-    //Check if the private key is an instance of Key
-    Assert.isInstanceOf(Key.class, privateKey);
+    //Check if the private key is an instance of RSAKey
+    Assert.isInstanceOf(RSAKey.class, privateKey);
   }
+
+  @Test
+  public void testGetKeypair()
+      throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException {
+    KeyStore instance = KeyStore.getInstance(KeyStore.getDefaultType());
+    instance.load(findFile(KEYSTORE_FILE), PASSWORD.toCharArray());
+    KeyPair keyPair = KeyConvertUtil.getKeyPair(instance, PRIVATE_KEY_ALIAS, PASSWORD);
+    //Check if the private key is an instance of Key
+    Assert.isInstanceOf(RSAKey.class, keyPair.getPrivate());
+
+  }
+
 
   private static InputStream findFile(final String file) {
     return KeyConvertUtilTest.class.getClassLoader().getResourceAsStream(file);
